45ef362ea04fc6972a3539df4a99f7a4c8ab5ecf
ABDULKERIM ADEM <150909470+abdu2030@users.noreply.github.com>
2025-04-17 11:25:18 +0300

    Update main.py

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -23,7 +23,7 @@ def decrypt(decry,shift_number):
             ori_tex += tex
     print(ori_tex)
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+
 if direction=="encode":
     encrypt(text,shift)
 valid=input("do you want to decrypt??")
@@ -33,11 +33,4 @@ if valid=="yes":
     shift_num=int(input("type the original shift number: "))
     decrypt(dec,shift_num)
 
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
-
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
-
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
 
