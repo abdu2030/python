28b61d72a386ea5ceee26189819ee1e12bce87b6
abdu2030 <abdukilade456@gmail.com>
2025-04-19 09:22:20 +0300

    new

---
+++ Day 9/Blind Auction Project/main.py
@@ -0,0 +1,27 @@
+# TODO-1: Ask the user for input
+from art import logo
+print(logo)
+more=True
+bid_collection = {}
+while more != False:
+    name = input("WHAT IS YOUR NAME? : ")
+    bid = int(input("WHAT IS YOUR BID? :$"))
+    bid_collection[name]=bid
+    approve = input("is there an an other bidders,\"yes\" or \"No\"?")
+    if approve=="yes":
+        more=True
+        print("\n" * 20)
+    elif approve=="no":
+        more=False
+values=0
+winner=""
+for key in bid_collection:
+    if bid_collection[key]>values:
+        values=bid_collection[key]
+        winner=key
+print(f"THE WINNER IS {winner} WITH THE BID OF ${values}")
+# TODO-2: Save data into dictionary {name: price}
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
+
+
