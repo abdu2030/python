68f41b0fa6a359beb14b95c35b516f98bcf3761b
ABDULKERIM ADEM <abdukilade456@gmail.com>
2026-05-25 11:33:32 +0300

    Day 20: Update Snake Game with scoreboard improvements and high score tracking

--- Day 16-100/Day 20/main.py
+++ Day 16-100/Day 20/main.py
@@ -34,13 +34,16 @@ while game_is_on:
         scoreboard.increase_score()
     #detect collision with the wall
     if snake.head.xcor() > 270 or snake.head.xcor() < -270 or snake.head.ycor() > 260 or snake.head.ycor() < -260:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+
     #detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+
+
 
 
 
--- Day 16-100/Day 20/scoreboard.py
+++ Day 16-100/Day 20/scoreboard.py
@@ -6,6 +6,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
+        #self.content = 0
         self.color("white")
         self.penup()
         self.goto(0, 250)
@@ -13,11 +15,19 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT,font=FONT)
+        self.clear()
+        with open("high_score.txt") as high:
+            content = high.read()
+        self.write(f"Score: {self.score} High Score: {content}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("high_score.txt",mode="w") as high:
+                high.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
